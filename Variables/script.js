97cdba7b84b474b49fa839a3d3e9933ba64b914a
venkatesh-khatake <venkateshkhatake8805@gmail.com>
2025-07-19 08:35:16 +0530

    initial update

--- Variables/script.js
+++ Variables/script.js
@@ -1,1 +1,32 @@
+// var a = 12;
+// {
+//     console.log(a);
+//     var a = 45;
+//     console.log(a)
+// }
+// console.log(a)
+
+// if(true){
+//     var a= 10;
+//     let b = 25;
+// }
+// console.log(a);
+// console.log(b)
+
+
+
+
+
+
+// let a = 45;
+// let b = "5";
+
+// let c = b + a;
+// console.log(c);
+// console.log(typeof(c))
+
+
+const arr = [1,3,4,5]
+console.log(...arr);
+console.log(arr);
 
